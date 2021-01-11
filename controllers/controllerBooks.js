7bbf25e29da1dd808aeeafee4ae21c1bef97818d
Pham Minh Duy <phamminhduy@pop-os.localdomain>
2021-01-11 16:34:52 +0700

    upload image bi cloudinary

--- controllers/controllerBooks.js
+++ controllers/controllerBooks.js
@@ -2,87 +2,66 @@ const mongoose = require('mongoose');
 const { ObjectID } = require('mongodb');
 const formidable = require('formidable');
 const fs = require('fs')
+const cloudinary = require('cloudinary').v2;
 
 const Book = require('../models/listBookModels')
 const Category = require('../models/listCategoryModels')
 
+cloudinary.config({
+  cloud_name: 'dvhhz53rr',
+  api_key: '272966692333936',
+  api_secret: '0Tz28aal-cHCjr0K5bYF4V00XYo'
+});
 exports.index = (req, res, next) => {
-    res.render('index', {
-        title: "Dashboard"
-    });
+  res.render('index', {
+    title: "Dashboard"
+  });
 }
 exports.books = async (req, res, next) => {
-    console.log("Loading books...")
-
-    let books = await Book.listBook();
-
-    console.log("Load books successfully!")
-
-    console.log("Loading categories")
-
-    let listCategory = await Category.findCategories();
-    let categories = []
-    listCategory.forEach(category => {
-        categories.push(category.name)
-    });
-
-    console.log("Load categories successfully!")
-
-    res.render('books', {
-        books,
-        categories,
-        title: "Books"
-    });
+  console.log("Loading books...")
+  let books = await Book.listBook();
+  console.log("Load books successfully!")
+  console.log("Loading categories")
+  let listCategory = await Category.findCategories();
+  let categories = []
+  listCategory.forEach(category => {
+    categories.push(category.name)
+  });
+  console.log("Load categories successfully!")
+  res.render('books', {
+    books,
+    categories,
+    title: "Books"
+  });
 }
 exports.addBook = async (req, res, next) => {
-    const form = formidable({ multiples: true });
-    let cover = [];
-    form.parse(req, async (err, fields, files) => {
-        if (err) {
-            next(err);
-            return;
-        }
-        console.log(__dirname + '/../public/image/books')
-        let image = []
-        if (files.cover1 && files.cover1.size > 0) {
-            const name = files.cover1.path.split('/').pop();
-            const extension = files.cover1.name.split('.').pop();
-            cover.push('img/' + name + '.' + extension);
-            const filePathCustomer = __dirname + '/../../finalProject-customer/public/book-shop/img/books/';
-            fs.renameSync(files.cover1.path, filePathCustomer + name + '.' + extension);
-            image.push('/book-shop/img/books/' + name + '.' + extension);
-        }
-        if (files.cover2 && files.cover2.size > 0) {
-            const name = files.cover2.path.split('/').pop();
-            const extension = files.cover2.name.split('.').pop();
-            cover.push('img/' + name + '.' + extension);
-
-            const filePathCustomer = __dirname + '/../../finalProject-customer/public/book-shop/img/books/';
-            fs.renameSync(files.cover2.path, filePathCustomer + name + '.' + extension);
-            image.push('/book-shop/img/books/' + name + '.' + extension);
-        }
-        if (files.cover3 && files.cover3.size > 0) {
-            const name = files.cover3.path.split('/').pop();
-            const extension = files.cover3.name.split('.').pop();
-            cover.push('img/' + name + '.' + extension);
-            const filePathCustomer = __dirname + '/../../finalProject-customer/public/book-shop/img/books/';
-            fs.renameSync(files.cover3.path, filePathCustomer + name + '.' + extension);
-            image.push('/book-shop/img/books/' + name + '.' + extension);
-        }
-        const book = {
-            name: fields.name,
-            basePrice: fields.basePrice,
-            description: fields.description,
-            coversString: image
-        };
-        try {
-            await Book.addOneBook(book);
-            res.redirect('/books');
-        }
-        catch (err) {
-            console.log(err);
-        }
-    });
+  const form = formidable({ multiples: true });
+  form.parse(req, async (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    let image = [];
+    const img1 = await cloudinary.uploader.upload(files.cover1.path);
+    const img2 = await cloudinary.uploader.upload(files.cover2.path);
+    const img3 = await cloudinary.uploader.upload(files.cover3.path);
+    image.push(img1.url);
+    image.push(img2.url);
+    image.push(img3.url);
+    const book = {
+      name: fields.name,
+      basePrice: fields.basePrice,
+      description: fields.description,
+      coversString: image
+    };
+    try {
+      await Book.addOneBook(book);
+      res.redirect('/books');
+    }
+    catch (err) {
+      console.log(err);
+    }
+  });
 }
 
 
@@ -95,13 +74,13 @@ exports.addBook = async (req, res, next) => {
 //     let tempFilter = {name: category}
 //     let categoryID = (await Category.findCategories(tempFilter))[0]._id
 
-    // const book = {
-    //     name: name,
-    //     category: category,
-    //     categoryID: categoryID,
-    //     basePrice: basePrice,
-    //     description: description
-    // };
+// const book = {
+//     name: name,
+//     category: category,
+//     categoryID: categoryID,
+//     basePrice: basePrice,
+//     description: description
+// };
 
 //     let temp = saveCovers(coversEncoded)
 
@@ -120,83 +99,83 @@ exports.addBook = async (req, res, next) => {
 //     }
 // }
 const saveCovers = (coversEncoded) => {
-    let coverTypes = []
-    let coversString = []
-    coversEncoded.forEach(coverEncoded => {
-        if (!coverEncoded) {
-            coverTypes.push(null)
-            coversString.push(null)
-            return;
-        }
-
-        const coverJSON = JSON.parse(coverEncoded)
-        if (coverJSON != null) {
-            let coverString = coverJSON.data;
-            let coverType = coverJSON.type;
-            coverTypes.push(coverType)
-            coversString.push(coverString)
-        }
-    });
-    return {
-        coverTypes,
-        coversString
+  let coverTypes = []
+  let coversString = []
+  coversEncoded.forEach(coverEncoded => {
+    if (!coverEncoded) {
+      coverTypes.push(null)
+      coversString.push(null)
+      return;
     }
+
+    const coverJSON = JSON.parse(coverEncoded)
+    if (coverJSON != null) {
+      let coverString = coverJSON.data;
+      let coverType = coverJSON.type;
+      coverTypes.push(coverType)
+      coversString.push(coverString)
+    }
+  });
+  return {
+    coverTypes,
+    coversString
+  }
 }
 exports.updateBook = async (req, res, next) => {
-    const id = req.body.id;
-    const name = req.body.newName;
-    const category = req.body.newCategory;
-    const coversEncoded = [req.body.newCover1, req.body.newCover2, req.body.newCover3];
-    const basePrice = req.body.newBasePrice;
-    const description = req.body.newDescription;
+  const id = req.body.id;
+  const name = req.body.newName;
+  const category = req.body.newCategory;
+  const coversEncoded = [req.body.newCover1, req.body.newCover2, req.body.newCover3];
+  const basePrice = req.body.newBasePrice;
+  const description = req.body.newDescription;
 
-    const filter = { _id: id };
+  const filter = { _id: id };
 
-    let tempFilter = { name: category }
-    categoryID = (await Category.findCategories(tempFilter))[0]._id
+  let tempFilter = { name: category }
+  categoryID = (await Category.findCategories(tempFilter))[0]._id
 
 
-    let update = {
-        name: name,
-        category: category,
-        categoryID: categoryID,
-        basePrice: basePrice,
-        description: description
-    };
+  let update = {
+    name: name,
+    category: category,
+    categoryID: categoryID,
+    basePrice: basePrice,
+    description: description
+  };
 
-    let temp = saveCovers(coversEncoded)
+  let temp = saveCovers(coversEncoded)
 
-    if (temp) {
-        const book = await Book.getOneBook(id)
-        update.coversString = []
-        update.coverTypes = []
-        for (let i = 0; i < 3; i++) {
-            if (!temp.coversString[i]) {
-                update.coversString.push(book.coversString[i])
-                update.coverTypes.push(book.coverTypes[i])
-            } else {
-                update.coversString.push(temp.coversString[i])
-                update.coverTypes.push(temp.coverTypes[i])
-            }
-        }
+  if (temp) {
+    const book = await Book.getOneBook(id)
+    update.coversString = []
+    update.coverTypes = []
+    for (let i = 0; i < 3; i++) {
+      if (!temp.coversString[i]) {
+        update.coversString.push(book.coversString[i])
+        update.coverTypes.push(book.coverTypes[i])
+      } else {
+        update.coversString.push(temp.coversString[i])
+        update.coverTypes.push(temp.coverTypes[i])
+      }
     }
+  }
 
-    try {
-        await Book.updateBook(filter, update);
-        res.redirect('/books');
-    }
-    catch (err) {
-        console.log(err);
-    }
+  try {
+    await Book.updateBook(filter, update);
+    res.redirect('/books');
+  }
+  catch (err) {
+    console.log(err);
+  }
 
 }
 exports.deleteBook = (req, res, next) => {
-    const id = req.body.id;
-    try {
-        Book.deleteOneBook(id);
-        res.redirect('/books');
-    }
-    catch (err) {
-        console.log(err);
-    }
+  const id = req.body.id;
+  try {
+    Book.deleteOneBook(id);
+    res.redirect('/books');
+  }
+  catch (err) {
+    console.log(err);
+  }
 }
